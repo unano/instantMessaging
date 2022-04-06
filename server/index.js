e3e0d5b617fed73c8d5e50c3461d0ca0f30acf0c
unano <919659233@qq.com>
2022-04-06 15:57:32 +0800

    modify file place

--- server/index.js
+++ server/index.js
@@ -8,6 +8,16 @@ require("dotenv").config();
 app.use(cors());
 app.use(express.json());
 
+mongoose.connect(process.env.MONGO_URL,{
+         useNewUrlParser: true,
+         useUnifiedtopology: true,
+    }).then(() =>{
+        crossOriginIsolated.log("DB connection secceeed");
+    })
+    .catch((err) =>{
+        console.log(err.message);
+    });
+
 const server = app.listen(process.env.PORT,()=>{
     console.log(`Server is running at ${process.env.PORT}`)
 });
